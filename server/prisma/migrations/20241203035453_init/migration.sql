d72172ac50e623875c3cadf692f234d1290965dd
justkuper <kbank1988@gmail.com>
2024-12-03 14:33:41 -0800

    Add initial project setup and seed data
    - Added VSCode settings for project configuration.
    - Added .gitignore file to exclude unnecessary files from version control.
    - Added various assets including logo and product images.
    - Added profile image for user profiles.
    - Added package-lock.json and package.json for managing project dependencies.
    - Added Prisma migrations for database schema initialization.
    - Added Prisma schema definition file.
    - Added seed script and seed data files for populating the database with initial data.
    - Added TypeScript configuration file for the server.
    - Added server entry point file (index.ts) to start the application.

---
+++ server/prisma/migrations/20241203035453_init/migration.sql
@@ -0,0 +1,54 @@
+/*
+  Warnings:
+
+  - You are about to drop the column `amout` on the `Expenses` table. All the data in the column will be lost.
+  - You are about to drop the column `uniCost` on the `Purchases` table. All the data in the column will be lost.
+  - You are about to drop the column `changePercent` on the `SalesSummary` table. All the data in the column will be lost.
+  - You are about to drop the `ExpensesSummary` table. If the table is not empty, all the data it contains will be lost.
+  - You are about to drop the `PurchasesSummary` table. If the table is not empty, all the data it contains will be lost.
+  - Added the required column `amount` to the `Expenses` table without a default value. This is not possible if the table is not empty.
+  - Added the required column `unitCost` to the `Purchases` table without a default value. This is not possible if the table is not empty.
+
+*/
+-- DropForeignKey
+ALTER TABLE "ExpenseByCategory" DROP CONSTRAINT "ExpenseByCategory_expenseSummaryId_fkey";
+
+-- AlterTable
+ALTER TABLE "Expenses" DROP COLUMN "amout",
+ADD COLUMN     "amount" DOUBLE PRECISION NOT NULL;
+
+-- AlterTable
+ALTER TABLE "Purchases" DROP COLUMN "uniCost",
+ADD COLUMN     "unitCost" DOUBLE PRECISION NOT NULL;
+
+-- AlterTable
+ALTER TABLE "SalesSummary" DROP COLUMN "changePercent",
+ADD COLUMN     "changePercentage" DOUBLE PRECISION;
+
+-- DropTable
+DROP TABLE "ExpensesSummary";
+
+-- DropTable
+DROP TABLE "PurchasesSummary";
+
+-- CreateTable
+CREATE TABLE "PurchaseSummary" (
+    "purchaseSummaryId" TEXT NOT NULL,
+    "totalPurchased" DOUBLE PRECISION NOT NULL,
+    "changePercentage" DOUBLE PRECISION,
+    "date" TIMESTAMP(3) NOT NULL,
+
+    CONSTRAINT "PurchaseSummary_pkey" PRIMARY KEY ("purchaseSummaryId")
+);
+
+-- CreateTable
+CREATE TABLE "ExpenseSummary" (
+    "expenseSummaryId" TEXT NOT NULL,
+    "totalExpenses" DOUBLE PRECISION NOT NULL,
+    "date" TIMESTAMP(3) NOT NULL,
+
+    CONSTRAINT "ExpenseSummary_pkey" PRIMARY KEY ("expenseSummaryId")
+);
+
+-- AddForeignKey
+ALTER TABLE "ExpenseByCategory" ADD CONSTRAINT "ExpenseByCategory_expenseSummaryId_fkey" FOREIGN KEY ("expenseSummaryId") REFERENCES "ExpenseSummary"("expenseSummaryId") ON DELETE RESTRICT ON UPDATE CASCADE;
